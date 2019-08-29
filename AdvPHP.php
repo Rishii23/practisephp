8d7b1e5d1a8b9207d7d56d72542770aee759d844
Rishabh <rishabhaggarwal23@gmail.com>
2019-08-29 22:15:08 +0530

    Advanced work

--- AdvPHP.php
+++ AdvPHP.php
@@ -1,61 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Form Page</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="style.css">
-<script src="index.js"> </script>
+    <script src="index.js"> </script>
 </head>
 
 <body>
-    	<!-- Object & Classes -->
-    	<!-- Getter Setter Functions  -->
-    	<!-- Class Variables  -->
-    	<!-- Static Variables -->
-    	<!-- Exception Handling -->
-    	<!-- Access Class Members,(Variables/functions) -->
-    	<!--Private Public access Modifiers-->
-    	<!--Class Functions Declaration, Defination-->
-        <!--  -->
-        <!-- OOPS Concept. W.r.t PHP & Implement on Xampp -->
-        <!--  -->
-        <!-- SQL -phpmyadmin -->
-    	<!-- Read(sql Select,sql Function,sql Join Subqueries,%like) /implement/delete/update -->
-        <!--  -->
-        <!-- Impliment PHp Concept. with DB -->
-        <!--  -->
-        <!-- Model View Controller -->
+    <!-- Object & Classes -->
+    <!-- Getter Setter Functions  -->
+    <!-- Class Variables  -->
+    <!-- Static Variables -->
+    <!-- Exception Handling -->
+    <!-- Access Class Members,(Variables/functions) -->
+    <!--Private Public access Modifiers-->
+    <!--Class Functions Declaration, Defination-->
+    <!--  -->
+    <!-- OOPS Concept. W.r.t PHP & Implement on Xampp -->
+    <!--  -->
+    <!-- SQL -phpmyadmin -->
+    <!-- Read(sql Select,sql Function,sql Join Subqueries,%like) /implement/delete/update -->
+    <!--  -->
+    <!-- Impliment PHp Concept. with DB -->
+    <!--  -->
+    <!-- Model View Controller -->
 
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-<a class="navbar-brand" href="/index.php">Welcome Page</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="collapsibleNavbar">
-<ul class="navbar-nav">
-<li class="nav-item">
-<a class="nav-link active" href="/AdvPHP.php">Advance PHP</a>
-</li>
-<li class="nav-item">
-<a class="nav-link " href="/form.php">Form</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="/test.php">test</a>
-</li>    
-</ul>
-</div>  
-</nav>
-<hr class="generic">
-<div class="multiArray">
-    
-    <?php
-    $cars = array
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <a class="navba r-brand" href="/index.php">Welcome Page</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/AdvPHP.php">Advance PHP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/form.php">Form</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/test.php">test</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <hr class="generic">
+    <div class="multiArray">
+
+        <?php
+
+        
+                    $cars = array
       (
       array("Volvo",22,18),
       array("BMW",15,13),
@@ -68,7 +71,7 @@
     echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
     echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
     ?>
-<?php 
+        <?php 
 $friends = array(
     array("rishabh",23,1995),
     array("Yogesh",24,1996),
@@ -86,7 +89,14 @@ for ($row =0; $row < 3 ; $row++)  {
     echo"</ul>";
 }  
 ?>
-</div>
+    </div>
+
+    <?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
 </body>
 
 
