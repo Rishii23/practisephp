7ef9ec48b166c1c136fd0a074db222c409030340
Rishabh <rishabhaggarwal23@gmail.com>
2019-09-01 22:47:18 +0530

    added header files

--- AdvPHP.php
+++ AdvPHP.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Form Page</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <script src="index.js"> </script>
-</head>
+<? include "header.php" ?> 
 
 <body>
     <!-- Object & Classes -->
@@ -99,5 +85,5 @@ echo "Today is " . date("l");
 ?>
 </body>
 
+<? include "footer.php" ?> 
 
-</html>
