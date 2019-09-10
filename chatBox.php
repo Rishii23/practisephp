feb2588f10beec5fde67bb10f839eaf0e4351621
Rishabh <rish.calinfo@gmail.com>
2019-09-10 18:01:57 +0530

    chat box plan

---
+++ chatBox.php
@@ -0,0 +1,41 @@
+<?php include "header.php" ?>
+<!--  -->
+<section class="Chat-box">
+    <div class="container">
+        <div class="box">
+        <div class="row">
+
+<div class="col-md-10">
+<div class="ChatPreviewRecieved">
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum error inventore beatae voluptas placeat quibusda 
+        <br> TEXT RECEIVED </p>
+</div>
+</div>
+
+<div class="col-md-2"> </div>
+<div class="col-md-2"> </div>
+
+<div class="col-md-10">
+
+<div class="ChatPreviewSent">
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum error inventore beatae voluptas placeat quibusdam explicabo  <br> TEXT SENT  </p>
+</div>
+</div>
+
+            <div class="col-lg-12">
+                <div class="ChatReply">
+                    <div class="content">
+                        <p> Chat here:   </p>
+                        <input type="text">
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+</div>
+</section>
+
+
+</body>
+
+<?php include "footer.php" ?>
--- header.php
+++ header.php
@@ -1,44 +1,49 @@
     <!DOCTYPE html>
-<html lang="en">
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Form Page</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Form Page</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="style.css">
 
-</head>
-<body>
+    </head>
+
+    <body>
 
 
 
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-<a class="navbar-brand" href="/practisephp/index.php">Home </a>
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+            <a class="navbar-brand" href="/practisephp/index.php">Home </a>
 
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
 
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="collapsibleNavbar">
-<ul class="navbar-nav">
-<li class="nav-item">
-<a class="nav-link" href="/practisephp/AdvPHP.php">Advance PHP</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="/practisephp/Login.php">Login</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="/practisephp/form.php">Form</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="/practisephp/test.php">test</a>
-</li>    
-</ul>
-</div>  
-</nav>
-<!--  -->
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/practisephp/AdvPHP.php">Advance PHP</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/practisephp/Login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/practisephp/form.php">Form</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/practisephp/test.php">test</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/practisephp/chatBox.php">Chat Box</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!--  -->
